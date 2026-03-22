c7f2ebdfb955e072749508dbc60eab1e4612ef2e
Anton S <hitman.soldirs@gmail.com>
2026-03-22 18:18:59 +0800

    beginning

---
+++ src/main/java/FinanceManager/FinanceProgram/FinanceProgramApplication.java
@@ -0,0 +1,13 @@
+package FinanceManager.FinanceProgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceProgramApplication.class, args);
+	}
+
+}
